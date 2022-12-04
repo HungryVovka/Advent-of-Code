e8a00ac87a6af0da829930ea7f300dd86ffa4642
HungryVovka <94602014+HungryVovka@users.noreply.github.com>
2022-12-05 04:15:14 +0500

    feat: advent of code 2022

--- 2022/Day 1 Calorie Counting/Day 1 Calorie Counting.py
+++ 2022/Day 1 Calorie Counting/Day 1 Calorie Counting.py
@@ -9,3 +9,19 @@ sum_calories = sorted(sum(i) for i in food)[::-1]
 
 print("Part One: %d" % sum_calories[0])
 print("Part Two: %d" % sum(sum_calories[:3]))
+
+# or
+
+all_calories = [i.strip() for i in open("input.txt")]
+calories = 0
+sum_calories = []
+for i in all_calories:
+    if i != "":
+        calories += int(i)
+    else:
+        sum_calories.append(calories)
+        calories = 0
+sum_calories = sorted(sum_calories)[::-1]
+
+print("Part One: %d" % sum_calories[0])
+print("Part Two: %d" % sum(sum_calories[:3]))
